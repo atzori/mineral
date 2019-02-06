c4a2130c572d34d7383ced932f0aa12e24a7e4dd
Maurizio Atzori <atzori@gmail.com>
2019-02-06 16:32:40 +0100

    minl function (loops)

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# minr
-Optimized recursion and aggregates in SPARQL via custom function
+mineral: Computing Optimized Recursive SPARQL Queries with Aggregates
+=====================================================================
+
+**NOTE: THIS IS A DEVELOPMENT VERSION (WORK IN PROGRESS)** 
+
+
+**mineral**, loosely for *min* (function) *e*nhanced *r*ecursion through *a*ccumu*l*ation is a custom SPARQL function for optimized recursion within SPARQL with aggregates, inspired by the work by Prof. [Carlo Zaniolo](http://web.cs.ucla.edu/~zaniolo/) on [*Fixpoint semantics and optimization of recursive Datalog programs with aggregates*](https://doi.org/10.1017/S1471068417000436) and work by Prof. [Maurizio Atzori](http://atzori.webofcode.org/) on [*Computing recursive SPARQL queries](https://doi.org/10.1109/ICSC.2014.54) (see on [github](https://github.com/atzori/runSPARQL)).
+
+
+
+Functions and usage
+-------------------
+We developed the following two functions (the `wfn` prefix stands for `<http://webofcode.org/wfn/>`):
+
+  - `wfn:minl(?query, [, ?i0, ?i1, ...])` - memoization is used to cut off loops and increase speed on already-seen states in the search space
+  - `wfn:mina(?query, ?a [, ?i0, ?i1, ...])` - Zaniolo's inspired technique is used to cut off unnecessary branches in the search space including loops (parameter `a` is used as an accumulator, see below)
+ 
+In both versions, optimization can be disabled via server configuration.
+
+
+Install and compile *(tested with Fuseki 3.8.0)*
+-------------------
+1. ensure you have OpenJDK 8 or later correctly installed
+2. download and extract [Apache Fuseki 2](https://jena.apache.org/download/#apache-jena-fuseki) on directory `./fuseki`
+3. compile Java source code of functions: `./compile`
+4. run fuseki using testing settings: `./run-fuseki`
+    - fuseki server settings in `config/config.ttl` 
+    - initial data in `config/dataset.ttl` 
+    - log4j settings in `config/log4j.properties`
+5. go to: [http://127.0.0.1:3030](http://127.0.0.1:3030)
+
+
+
+Examples
+--------
+
+### Computing the Factorial
+In the following it is shown how to compute the factorial of 3.
+
+With **minl**:
+
+    PREFIX : <http://webofcode.org/wfn/>
+    
+    SELECT ?result { 
+        # bind variables to parameter values 
+        VALUES ?query { "BIND ( IF(?i0 <= 0, 1, ?i0 * :minl(?query, ?i0-1)) AS ?result)" }
+
+        # actual call of the recursive query 
+        BIND(:minl(?query, 3) AS ?result)
+    } 
+
+
+With **mina**:
+
+    PREFIX : <http://webofcode.org/wfn/>
+    
+    SELECT ?result { 
+        # bind variables to parameter values 
+        VALUES ?query { "BIND ( IF(?i0 <= 0, ?n, :mina(?query, ?a * ?i0, ?i0-1)) AS ?result)" }
+
+        # actual call of the recursive query 
+        BIND(:mina(?query, 1, 3) AS ?result)
+    } 
+
+
+
+### Graph search: shortest distance between 2 nodes
+In the following it is shown how to compute the shortest distance between two nodes ([dbo:PopulatedPlace](http://dbpedia.org/ontology/PopulatedPlace) and [dbo:Village](http://dbpedia.org/ontology/Village)).
+
+With **minl**:
+
+    PREFIX : <http://webofcode.org/wfn/>
+
+    SELECT ?result { 
+        # bind variables to parameter values 
+        VALUES ?query { 
+            "OPTIONAL { ?i0 <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next } BIND( IF(?i0 = <http://dbpedia.org/ontology/PopulatedPlace>, 0 , 1 + wfn:minl(?query, ?next)) AS ?result)" }
+
+        # actual call of the recursive query 
+        BIND( :minl(?query, <http://dbpedia.org/ontology/Village>) AS ?result)
+    } 
+
+
+
+With **mina**:
+
+    PREFIX wfn: <http://webofcode.org/wfn/>
+
+    SELECT ?result { 
+        # bind variables to parameter values 
+        VALUES ?query { 
+            "OPTIONAL { ?i0 <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next } BIND( IF(?i0 = <http://dbpedia.org/ontology/PopulatedPlace>, ?a, :mina(?query, ?n+1, ?next)) AS ?result)" }
+
+        # actual call of the recursive query 
+        BIND( :mina(?query, 0, <http://dbpedia.org/ontology/Village>) AS ?result)
+    } 
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/webofcode/wfn/minl.java
@@ -0,0 +1,204 @@
+/* 
+ *  Copyright 2014-2019. Maurizio Atzori <atzori@unica.it>
+ *
+ */
+ 
+package org.webofcode.wfn;
+
+import org.apache.jena.sparql.function.*;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
+import org.apache.jena.sparql.function.FunctionRegistry;
+
+
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.sparql.expr.Expr;
+
+import org.apache.jena.sparql.ARQConstants;
+import org.apache.jena.rdfconnection.*;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.HttpClient;
+import org.apache.jena.graph.NodeFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.*;
+import static java.util.stream.Collectors.*;
+
+import org.apache.jena.atlas.lib.Lib;
+import org.apache.jena.query.QueryBuildException;
+import org.apache.jena.sparql.ARQInternalErrorException;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.ExprList;
+
+/**
+ * This class implements the minl function to be used within SPARQL queries in the Apache Fuseki 2 triplestore.
+*/
+public class minl extends FunctionBase {
+    static Logger log = LoggerFactory.getLogger(minl.class);
+    
+    private final static boolean CACHE_ENABLED = true;
+    static final String ENDPOINT = System.getProperty("endpoint");
+    static final String FN_NAME = "http://webofcode.org/wfn/minl";
+    static int nInstances = 0;
+    static Map<String,NodeValue> cache = new ConcurrentHashMap<>();
+    static final NodeValue VISITED = NodeValue.makeNode(NodeFactory.createBlankNode("VISITED"));
+    static  String TEMPLATE;
+
+
+    public static void init() {
+        log.info("Registering function {}", FN_NAME);
+        FunctionRegistry.get().put(FN_NAME, minl.class);
+        //log.debug(System.getProperties().toString());
+        log.info("Endpoint set to {}", ENDPOINT);   
+        
+        // load sparql template file     
+        try {
+            TEMPLATE = String.join("\n", Files.readAllLines(Paths.get(System.getProperty("sparql_template"))));
+        } catch(Exception e) {
+            log.error("Error while reading sparql template");
+        }
+        //log.debug("Template set to {}", TEMPLATE);
+        
+    }
+    public minl() {
+        nInstances += 1;
+        //log.debug("Creating instance #{}", nInstances);
+    }
+
+
+    @Override
+    public void checkBuild(String uri, ExprList args) { 
+        if ( args.size() < 1 )
+            throw new QueryBuildException("Function '"+Lib.className(this)+"' takes at least one argument (a query snippet)") ;
+    }
+
+    public NodeValue exec(List<NodeValue> args) { 
+    
+        if ( args == null )
+            // The contract on the function interface is that this should not happen.
+            throw new ARQInternalErrorException(Lib.className(this)+": Null args list") ;
+        
+        if ( args.size() < 1 )
+            throw new ExprEvalException(Lib.className(this)+": Wrong number of arguments: Wanted 1+, got "+args.size()) ;
+        
+
+        String querySnippet = args.get(0).toString();
+        List<NodeValue> fnArgs = new ArrayList<>(args);
+        fnArgs.remove(0);
+        log.info("Calling with {}", fnArgs);
+            
+
+        String varNames = IntStream.range(0,fnArgs.size()).mapToObj(i -> "?i"+i).collect(Collectors.joining(" "));
+        String varValues = args.stream().map(e -> e.toString()).collect(Collectors.joining("\n"));
+
+        String bindings = String.format("VALUES (?query %s) { (\n%s\n)}", varNames, varValues);
+        
+        String query = TEMPLATE
+            .replace("%query_snippet%", querySnippet.trim().replaceAll("^\\\"(.*)\\\"$","$1"))
+            .replace("%bindings%", bindings);
+                    
+                
+        NodeValue result = null;  
+
+        // try to avoid loops    
+        String key = String.join("|",args.stream().map(e->e.toString()).collect(toList())); //String.format("%s|%s", i0, query); // endpoint
+        if (CACHE_ENABLED) {
+            if(cache.containsKey(key)) {
+                result = cache.get(key); 
+                log.info("Loop found for i0={}, result={}", fnArgs, result);
+                return result==VISITED? nodeNone(): result; 
+            } else 
+                cache.put(key, VISITED); // mark as "visited (but not resolved yet)" 
+        }
+        
+        List<RDFNode> solutions = executeQuery(query, ENDPOINT); //service); 
+        //log.info("Result size for i0={} was {}, solutions= {}", i0, solutions.size(), solutions);
+        
+        if (solutions.size()>0) {
+            RDFNode node = solutions.get(0); // only the first solution is used
+            result =(node == null) ? nodeNone() : NodeValue.makeNode(node.asNode()); // check why node can/could(?) be null
+        } else {
+	        log.warn("Resultset was empty");
+		    result = nodeNone(); //nvNothing; // Expr.NONE; //NodeValue.makeString("no result!");
+        }
+
+	    //log.info("Result was {}", result);
+	    log.info("Result for {} was {}, solutions={}", fnArgs, result, solutions);
+        if(CACHE_ENABLED) cache.put(key, result);
+        return result;        
+
+    }
+    
+    private static List<RDFNode> executeQuery(String query, String service) {
+    
+        //QueryExecution qe = QueryExecutionFactory.create(query); // local call not working without specifying a dataset        
+        //log.info(""+ARQConstants.sysCurrentDataset ); //symDatasetDefaultGraphs.toString()) ;
+
+
+	    List<RDFNode> solutions = null;
+       /* try ( RDFConnection conn = RDFConnectionFactory.connect("http://127.0.0.1:3030/ds") ) {
+
+            log.info("within try x1");
+
+            ResultSet rs = conn.query(query).execSelect() ;
+            log.info("within try x2");
+            solutions = resultSet2List(rs);
+            //return ResultSetFactory.copyResults(rs) ;
+
+
+        }*/
+
+
+        /*
+            An HttpClient is necessary since default client only accepts a limited number of connections (no more than 5)
+            https://stackoverflow.com/questions/49661698/how-can-i-change-the-number-of-connections-par-route-in-jena-more-than-5
+        */
+        HttpClient client = HttpClientBuilder.create().build();
+	    
+	    try (QueryExecution qe = 
+	            QueryExecutionFactory.sparqlService(service, query, client)
+	            //QueryExecutionFactory.create(query) //, Dataset.getDefaultModel()) 
+	      ) {
+	      
+            ResultSet rs = qe.execSelect(); // sparql query is actually executed remotely on the endpoint
+            //rs = ResultSetFactory.copyResults(rs) ;
+            solutions = resultSet2List(rs);            
+        }
+        return solutions;
+    
+    }
+    
+    //@SuppressWarnings( "deprecation" )
+    private static NodeValue nodeNone() {
+        //return (NodeValue) NodeValue.NONE;
+        //return NodeValue.nvNothing; // equivalent to: NodeValue.makeNode(NodeFactory.createBlankNode(strForUnNode)) 
+        //return NodeValue.NONE.eval(null,null);
+        return NodeValue.makeNode(NodeFactory.createBlankNode("noResult")); // this is working
+        //return new NodeValueInteger(-999);
+    }
+    
+    /** convert a ResultSet with column "result" into a List of RDFNode */
+    private static List<RDFNode> resultSet2List(ResultSet rs) {
+        if (rs == null) throw new RuntimeException("null as result");
+        
+        List<RDFNode> solutions = new ArrayList<>();	        
+	    	        
+        while(rs.hasNext()) {
+	        QuerySolution res = rs.next();
+	        if(res == null) throw new RuntimeException("querysolution was null");
+	        RDFNode node = res.get("result");
+	        solutions.add(node);	        
+        }
+        
+        
+        return solutions;
+    }        
+
+}
